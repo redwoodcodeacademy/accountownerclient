8540de9c1a3ba0e16bec69848bee595b89e03dfc
redwoodcodeacademy <info@redwoodcodeacademy.com>
2020-11-02 11:33:50 -0800

    make it better

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://localhost:5000',
-    baseURL: 'http://brokerage.herokuapp.com',
+    baseURL: 'https://brokerage.herokuapp.com',
     headers: {
         headerType: 'example header type'
     }
